b875273d1d11a75a44a1ab0b3086c41148c0d4f2
Ali hamza Shabbir <160996641+AliHamza-lab@users.noreply.github.com>
2025-02-17 01:36:21 +0500

    Commit #1

---
+++ video 2/video 2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include<string>
+int main(){
+	// this is integer data type
+  /*  this is the integer
+     this is the integer
+      this is the integer
+	
+*/
+// varibles in c++
+    float b=3.587878787878787;
+    double u=9.859869589568958;
+    bool a=false;
+    string cn="code cracker";
+    char h='a';
+    int n=-1;
+    cout<<b<<"\n";
+    cout<<u<<"\n";
+    cout<<a<<"\n";
+    cout<<cn<<"\n";
+    cout<<h<<"\n";
+    cout<<n<<"\n";
+    
+    
+}
