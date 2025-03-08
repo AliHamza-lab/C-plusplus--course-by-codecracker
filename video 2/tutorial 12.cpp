75f57ca8945eaef39b6d5321ac0d194700e73588
Ali hamza Shabbir <160996641+AliHamza-lab@users.noreply.github.com>
2025-03-09 03:00:03 +0500

    Commit #1

---
+++ video 2/tutorial 12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void add(int a,int b){
+	cout<<a+b;
+}
+
+//void add(double c,double d){
+//	cout<<c+d;
+//}
+
+int main(){
+	add(1.2,2.2);
+	cout<<"\n";
+	add(2.3,4.5);
+}
